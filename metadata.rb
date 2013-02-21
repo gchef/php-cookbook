c2f05dc0d7637e83ee6898cc22b9c3a93c24059e
Gerhard Lazu <gerhard@lazu.co.uk>
2013-02-21 03:40:59 +0000

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Gerhard Lazu"
 maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
 description       "PHP5 recipe"
-version           "0.2.0"
+version           "0.3.0"
 
 recipe "php", "Installs/uninstalls PHP5 packages and provides the default php.ini"
 
